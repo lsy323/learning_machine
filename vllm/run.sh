47c169ba1f07ba9e0d2f3d0ea68ba9b6ee799d35
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-03-17 14:11:33 -0700

    Update run.sh

--- vllm/run.sh
+++ vllm/run.sh
@@ -11,11 +11,11 @@ export HF_TOKEN=xxx # chaneg to your HF_token
 
 # PyTorch/XLA simple test case
 export PJRT_DEVICE=TPU
-python -c "import torch; import torch_xla; import torch_xla.runtime as xr; print(xr.device_type())"
-python -c "import torch; import torch_xla; import torch_xla.runtime as xr; print(torch_xla._XLAC._xla_get_devices())"
-python examples/offline_inference/tpu.py # failed with error: `RuntimeError: Bad StatusOr access: UNKNOWN: TPU initialization failed: `
+# python -c "import torch; import torch_xla; import torch_xla.runtime as xr; print(xr.device_type())"
+# python -c "import torch; import torch_xla; import torch_xla.runtime as xr; print(torch_xla._XLAC._xla_get_devices())"
+# python examples/offline_inference/tpu.py # failed with error: `RuntimeError: Bad StatusOr access: UNKNOWN: TPU initialization failed: `
 vllm serve meta-llama/Meta-Llama-3.1-8B --swap-space 8 --disable-log-requests --tensor_parallel_size=4 --max-model-len=2048 --num-scheduler-steps=1 --port 6089 # hang
 
-# JAX matched function
-export JAX_PLATFORMS='tpu'
-python -c "import jax; print(jax.devices()); print(jax.devices()[0]); print(jax.devices()[0].memory_stats)"
+# # JAX matched function
+# export JAX_PLATFORMS='tpu'
+# python -c "import jax; print(jax.devices()); print(jax.devices()[0]); print(jax.devices()[0].memory_stats)"
